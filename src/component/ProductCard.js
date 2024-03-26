b281b7a7e2ee7b95306543aa111a678289f7a663
장 재 훈 <peng2412@naver.com>
2024-03-27 02:06:23 +0900

    [Add] -> Header + Navbar + json-server

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+    return (
+        <div>
+            <img width='300px' height='400px' src='https://satur.co.kr/web/product/medium/202403/1b03a86b8257f4a6ebf5437648b9f863.jpg' />
+            <div>Conscious choice</div>
+            <div>벨티드 트윌 코트</div>
+            <div>$99900</div>
+            <div>신제품</div>
+        </div>
+    )
+}
+
+export default ProductCard
